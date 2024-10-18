b5bf700e0e8f51788c564b656d91b68a8643b323
ki8vi <oneofbln@gmail.com>
2024-10-19 01:54:46 +0500

    chore: setup dependencies

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { cpus } from 'os';
+import http from 'http'
+
+const server = http.createServer((req, res) => {
+
+})
+console.log(cpus().length)
+console.log('hello it workssdf')
+const aasd = 3;
+console.log(aasd);
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+
+module.exports = {
+  target: 'node',
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'start.js',
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ]
+  }
+};
